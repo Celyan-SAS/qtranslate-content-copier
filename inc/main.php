3dc1018c9918fe9ef189ddf47f42e48fd76c4fcc
yannbug <yann@abc.fr>
2016-12-08 11:06:20 +0100

    Add bulk action in footer

--- inc/main.php
+++ inc/main.php
@@ -14,8 +14,29 @@ class wpqTCC {
 	 */
 	public function __construct() {
 		
+		/**
+		 * Add bulk action to copy content over in all languages
+		 *
+		 */
+		add_action( 'admin_footer', array( $this, 'addBulkActionInFooter' ) );		
 	}
 	
-	
+	/**
+	 * Adds a new bulk action to the WP content list screens
+	 * To copy content over in all qTranslate languages
+	 *
+	*/
+	public function addBulkActionInFooter() {
+		?>
+		<script>
+		(function($) {
+			$(document).ready(function() {
+				$('<option>').val('wpqtcc_dupe').text('<?php _e( 'Copy in all languages', 'wpqtcc' ); ?>').appendTo("select[name='action']");
+				$('<option>').val('wpqtcc_dupe').text('<?php _e( 'Copy in all languages', 'wpqtcc' ); ?>').appendTo("select[name='action2']");
+			});
+		})( jQuery );
+		</script>
+		<?php 
+	}
 }
 ?>
