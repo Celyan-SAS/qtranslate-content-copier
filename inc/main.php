fd65b5b25be798e788a3746b995ecf785c2a3fe1
yannbug <yann@abc.fr>
2016-12-08 10:57:05 +0100

    Initial commit

---
+++ inc/main.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * WordPress qTranslate content copier plugin main class
+*
+* @author yann@abc.fr
+* @see: https://github.com/Celyan-SAS/qtranslate-content-copier
+*
+*/
+class wpqTCC {
+	
+	/**
+	 * Class constructor
+	 *
+	 */
+	public function __construct() {
+		
+	}
+	
+	
+}
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *	@package qTranslate content copier
+ *	@author Celyan
+ *	@version 0.0.1
+ */
+/*
+ Plugin Name: qTranslate content copier
+ Plugin URI: https://github.com/Celyan-SAS/qtranslate-content-copier
+ Description: Will copy content over from one language to another in a qTranslate-enabled WordPress site
+ Version: 0.0.1
+ Author: Yann Dubois
+ Author URI: http://www.yann.com/
+ License: GPL2
+ */
+
+include_once( dirname(__FILE__) . '/inc/main.php' );
+
+new wpqTCC();
